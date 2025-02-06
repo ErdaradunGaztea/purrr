afad467eb2646d1dd886bb4a09a15b4ab6ee4132
Laura <erdaradun.gaztea+github@gmail.com>
2025-02-06 12:01:01 +0100

    implement optimal every()

--- src/every.c
+++ src/every.c
@@ -1,13 +1,17 @@
 #define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
+#include <stdbool.h>
+
+#include "conditions.h"
+
+bool test_bool_out(SEXP value) {
+  return TYPEOF(value) == LGLSXP && Rf_length(value) == 1 && LOGICAL(value)[0] != NA_LOGICAL;
+}
 
 SEXP every_impl(SEXP env, SEXP ffi_n, SEXP ffi_i) {
   int n = INTEGER_ELT(ffi_n, 0);
   int* p_i = INTEGER(ffi_i);
-
-  SEXP out = PROTECT(Rf_allocVector(LGLSXP, 1));
-  LOGICAL(out)[0] = TRUE;
 
   SEXP x_sym = Rf_install(".x");
   SEXP p_sym = Rf_install(".p");
@@ -17,6 +21,10 @@ SEXP every_impl(SEXP env, SEXP ffi_n, SEXP ffi_i) {
   SEXP x_i_sym = PROTECT(Rf_lang3(R_Bracket2Symbol, x_sym, i_sym));
   SEXP call = PROTECT(Rf_lang3(p_sym, x_i_sym, R_DotsSymbol));
 
+  SEXP out = PROTECT(Rf_allocVector(LGLSXP, 1));
+  int* p_out = LOGICAL(out);
+  *p_out = TRUE;
+
   for (int i = 0; i < n; i++) {
     *p_i = i + 1;
 
@@ -25,18 +33,25 @@ SEXP every_impl(SEXP env, SEXP ffi_n, SEXP ffi_i) {
     }
 
     SEXP res = PROTECT(R_forceAndCall(call, 1, env));
-    int res_value = LOGICAL_ELT(res, 0);
+
+    if (!test_bool_out(res)) {
+      r_abort(
+        "%s must be a numeric vector, character vector, or function, not %s.",
+        "{.arg {error_arg}}",
+        rlang_obj_type_friendly_full(res, true, false)
+      );
+    }
+
+    int res_value = LOGICAL(res)[0];
+    UNPROTECT(1);
 
     if (res_value == FALSE) {
-      LOGICAL(out)[0] = FALSE;
-      UNPROTECT(1);
+      *p_out = FALSE;
       break;
     }
     if (res_value == NA_LOGICAL) {
-      LOGICAL(out)[0] = NA_LOGICAL;
+      *p_out = NA_LOGICAL;
     }
-
-    UNPROTECT(1);
   }
 
   *p_i = 0;
